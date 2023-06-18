09023ccea807ad78f30021faeb695cc97372dd87
Makar0kin <ilya.makarenko2004@gmail.com>
2023-06-18 20:22:39 +0300

    implement: worker_threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import url from 'url';
+import path from 'path';
+import os from 'os'
+import { Worker } from "worker_threads"
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    let workers = []
+    for (let i = 0; i < os.cpus().length; i++) {
+        workers.push(new Promise((resolve, reject) => {
+            let worker = new Worker(path.join(__dirname, 'worker.js'))
+            worker.postMessage({ n: i + 10 })
+            worker.on('message', ({status, data}, err) => {
+                if (!err) resolve({status, data})
+                else reject(err)
+                worker.terminate()
+            })
+        }))
+    }
+    Promise.allSettled(workers).then(results => {
+        console.log(results.map(({status, value}) => value))
+    })
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,29 @@
+import { parentPort } from "worker_threads"
+
+// worker.js - extend given function to work with data received from main thread and
+// implement function which sends result of the computation to the main thread
+// n should be received from main thread
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', ({n}) => {
+        // parentPort.postMessage(nthFibonacci(n))
+        let data;
+        try {
+            data = nthFibonacci(n)
+            parentPort.postMessage({
+                status: 'resolved', 
+                data: data 
+            })
+        } catch (error) {
+            parentPort.postMessage({
+                status: 'error', 
+                data: null 
+            })
+        }
+    })
 };
 
 sendResult();
